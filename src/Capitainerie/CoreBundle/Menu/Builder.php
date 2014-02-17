1ee74be54b2d36809cd2829c3ee004fa26a5b4c5
Sebastien Hordeaux <shordeaux@waterproof.fr>
2014-02-17 21:40:05 +0100

    tweask

--- src/Capitainerie/CoreBundle/Menu/Builder.php
+++ src/Capitainerie/CoreBundle/Menu/Builder.php
@@ -10,16 +10,16 @@ class Builder extends ContainerAware {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
 
-        $homeMenu = $menu->addChild('A propos');
-    	$homeMenu->addChild('Qui sommes nous?', array('route' => 'homepage'));
-    	$homeMenu->addChild('Qu\'est ce que le coworking?', array('route' => 'homepage'));
+        $homeMenu = $menu->addChild('A propos', array('route' => 'homepage'));
+    	//$homeMenu->addChild('Qui sommes nous?', array('route' => 'homepage'));
+    	//$homeMenu->addChild('Qu\'est ce que le coworking?', array('route' => 'homepage'));
     	$placeMenu = $menu->addChild('Le lieu', array('route' => 'place'));
         $placeMenu->addChild('Découvez le lieu', array('route' => 'place'));
         $placeMenu->addChild('Le sous marin', array('route' => 'sous-marin'));
         $placeMenu->addChild('Le port', array('route' => 'port'));
         $placeMenu->addChild('La salle de réunion', array('route' => 'salle-reunion'));
         $placeMenu->addChild('Le bar', array('route' => 'bar'));
-        $placeMenu->addChild('Accès', array('route' => 'homepage'));
+        //$placeMenu->addChild('Accès', array('route' => 'homepage'));
 
         $menu->addChild('Tarifs', array('route' => 'prices'));
         $menu->addChild('Co-workers', array('route' => 'team'));
